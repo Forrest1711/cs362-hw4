b9d01de74d0a3ca171e71c028d10c7774dda262c
Forrest <forrest.90h@gmail.com>
2021-04-29 21:39:02 -0700

    average of elements complete

--- avgElemTest.py
+++ avgElemTest.py
@@ -4,13 +4,14 @@ import unittest
 
 class avgElemTest(unittest.TestCase):
     def test1(self):
-        self.assertEqual(averageOfElements.avgElem([]), 69)
+        self.assertEqual(averageOfElements.avgElem(
+            [4, 13, 26, 69, 119, 183]), 69)
 
     def test2(self):
-        self.assertEqual(averageOfElements.avgElem([]), -420)
+        self.assertEqual(averageOfElements.avgElem([-10000, -420, 9160]), -420)
 
     def test3(self):
-        self.assertEqual(averageOfElements.avgElem([]), 666)
+        self.assertEqual(averageOfElements.avgElem([]), 0)
 
 
 if __name__ == '__main__':
